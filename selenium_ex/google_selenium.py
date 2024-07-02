33b490bc1a19faef9a79ba7824c45c4c52bff092
An01s <an.sabek01@gmail.com>
2024-07-02 09:16:12 +0300

    Exercise 26.6.24 update

---
+++ selenium_ex/google_selenium.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+# infra    # logic
+driver.get("https://www.google.com/")
+
+# logic
+search_input = driver.find_element(By.XPATH, "//*[@id='APjFqb']")
+search_input.send_keys("Python programming")
+search_input.send_keys(Keys.RETURN)
+
+
+first_result = driver.find_element(By.CSS_SELECTOR, "h3")
+print(f"The result is: \n{first_result.text}")
+
+
+# infra
+driver.quit()
