c60600f6b0c4332917ce45e2cd05424bf38dea4d
An01s <an.sabek01@gmail.com>
2024-07-18 09:14:14 +0300

    API_task_cards update

---
+++ API_task_cards/logic/api/api/api_add_two_jokers.py
@@ -0,0 +1,16 @@
+import requests
+
+from infra.api.api_wrapper import APIWrapper
+from infra.api.response_wrapper import ResponseWrapper
+from infra.both.config_provider import ConfigProvider
+
+
+class APIAddTwoJokers:
+    def __init__(self, request: APIWrapper):
+        self._request = request
+        self.config = ConfigProvider.load_from_file('../../config.json')
+
+    def api_post_add_two_jokers(self, data):
+        response = requests.post(f"{self.config['base_url']}/api/deck/new/?{data}")
+        response_wrapper = ResponseWrapper(ok=response.ok, status_code=response.status_code, body=response.json())
+        return response_wrapper
---
+++ API_task_cards/test/api/api/test_api_shuffle_cards.py
@@ -0,0 +1,33 @@
+import unittest
+from infra.api.api_wrapper import APIWrapper
+from logic.api.api_shuffle_the_cards import APIShuffle
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        api_request = APIWrapper()
+        self.api_shuffle = APIShuffle(api_request)
+
+    def test_shuffle_the_cards_get(self):
+        """
+        this function tests shuffling cards with GET
+        """
+        result = self.api_shuffle.get_shuffle_the_deck_get()
+        body = result.json()
+        self.assertTrue(result.ok)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(body["remaining"], 52)
+
+    def test_shuffle_the_cards_post(self):
+        """
+        this function tests shuffling cards with POST
+        """
+        result = self.api_shuffle.get_shuffle_the_deck_post()
+        self.assertTrue(result.ok)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.body['remaining'], 52)
+
+
+if __name__ == '__main__':
+    unittest.main()
