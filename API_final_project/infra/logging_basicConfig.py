ca208c41b0406421c7c131ea4a65417c2322197c
An01s <an.sabek01@gmail.com>
2024-07-23 13:42:41 +0300

    API_final_project after presentation with notes from our teachers

--- API_final_project/infra/logging_basicConfig.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-class LoggingSetup:
-    """
-    this function is to call the setUp in every page rather the building it everytime.
-    """
-    logging.basicConfig(filename='../api_final_project.log', level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%y %H:%M:%S')
